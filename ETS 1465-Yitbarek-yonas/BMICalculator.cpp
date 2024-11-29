a702c244beb1fe7302c12ba37ebcd94935c570a4
Yitbarek Yonas <yitbarekyonas14@gmail.com>
2024-11-29 13:31:23 -0800

    Update BMICalculator.cpp

--- ETS 1465-Yitbarek-yonas/BMICalculator.cpp
+++ ETS 1465-Yitbarek-yonas/BMICalculator.cpp
@@ -1,24 +1,19 @@
 /* Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
 in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to
-work for multiple person before terminated.*/
+work for multiple person before terminated and also give three chances for invalid input */
 
 #include <iostream>
-
-
 using namespace std;
-
 int main(){
 float weight, height, bmi;
     char choice;
     cout << "*************BMI CALCULATOR**********************" << endl;
     do {
          int counter = 0;
-        
-    trial1:
+         trial1:
     cout << "Enter the person's weight in kg: ";
             cin >> weight;
-           
-if (cin.fail() || weight <= 0) {
+           if (cin.fail() || weight <= 0) {
                 cin.clear();              // Clear the error state
                 cin.ignore(1000, '\n');   // Discard invalid input up to 1000 charachter or stops when the newline (\n) is encountered.
                 counter++;
@@ -30,20 +25,13 @@ if (cin.fail() || weight <= 0) {
                 if(counter == 3){
                    return 1; // Exit the program
                 }
-                
-                    
-                
-  
                  goto trial1;
 }
              int counter1 = 0;
-        
-        
-    trial2:
+         trial2:
     cout << "Enter the person's height in kg: ";
             cin >> height;
-           
-if (cin.fail() || height <= 0) {
+        if (cin.fail() || height <= 0) {
                 cin.clear();              // Clear the error state
                 cin.ignore(1000, '\n');   // Discard invalid input
                 counter1++;
@@ -51,8 +39,6 @@ if (cin.fail() || height <= 0) {
                 cout << "Invalid input. You have  " << (3 - counter1) ;
                 cout    << " chances left! Please enter a valid height.\n";
                 }
-                
-             
                 if(counter1 == 3){
                    return 1;
                 }
@@ -67,12 +53,8 @@ if (cin.fail() || height <= 0) {
 
         // Calculate BMI
         bmi = weight / (height * height);
-
-       
-    
-        cout << "Your BMI is: " << bmi << endl;
-
-        if (bmi < 18.5) {
+cout << "Your BMI is: " << bmi << endl;
+if (bmi < 18.5) {
             cout << "Weight category: Underweight" << endl;
         } else if (bmi >= 18.5 && bmi <= 24.9) {
             cout << "Weight category: Normal weight" << endl;
@@ -87,7 +69,5 @@ if (cin.fail() || height <= 0) {
     } while (choice == 'c');
     
      cout << "************************************************"; 
-
-    
     return 0;
     }
