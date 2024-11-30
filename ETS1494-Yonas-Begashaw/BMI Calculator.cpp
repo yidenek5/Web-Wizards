808cb8c363cb79ee4bc07115f3c63981efcfb9f8
Yonas Begashaw <yonas.begashaw@aastustudent.edu.et>
2024-11-30 13:09:18 +0300

    Create BMI Calculator.cpp

---
+++ ETS1494-Yonas-Begashaw/BMI Calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+float mass,height,BMI;
+char response;
+
+  label:
+    cout << "Enter your mass in kilograms" << endl;
+    cin >> mass;
+    cout << "Enter your height in meters" << endl;
+    cin >> height;
+    BMI = mass / (height * height);
+    cout << "Your BMI is " << BMI << endl;
+     
+    if (BMI < 18.5) {
+        cout << "You are underweight." << endl;
+    }else if (BMI >= 18.5 && BMI <25) {
+        cout << "You have a healthy weight." << endl;
+    }else if (BMI >= 25) {
+        cout << "You are overweight.";   
+    } 
+
+cout <<"If you want to calculate again type Y if not type N" << endl;
+cin >> response;
+if (response == 'Y') {
+    goto label;
+} else if (response == 'N') {
+    cout <<"Exiting program";
+} else {
+    cout <<"Invalid input, exiting program."<< endl;
+}
+
+return 0;
+} 
