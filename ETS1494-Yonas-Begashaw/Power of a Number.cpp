a33a9f6fa3e5707a9093d65bc398748856189a64
Yonas Begashaw <yonas.begashaw@aastustudent.edu.et>
2024-12-01 21:49:38 +0300

    Update Power of a Number.cpp

--- ETS1494-Yonas-Begashaw/Power of a Number.cpp
+++ ETS1494-Yonas-Begashaw/Power of a Number.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 int main () {
+  
 double fuel_tank, miles_per_gallon, miles;
+  
 cout << "Enter the fuel tank capacity in gallons" << endl;
 cin >> fuel_tank;
 cout << "Enter the miles per gallon" << endl;
 cin >> miles_per_gallon;
+  
 miles = miles_per_gallon * fuel_tank;
 cout << "The number of miles the automobile can be driven without refueling is " << miles;
+
 return 0;
 }
